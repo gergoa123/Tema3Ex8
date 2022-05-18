2b315cbd6cef41efc9bebab329c219ad0e6e5e55
Gergoa123 <gergo3124@gmail.com>
2022-05-18 19:52:49 +0300

    Add project files.

---
+++ Tema3Ex8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tema3Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul: ");
+            int numar = int.Parse(Console.ReadLine());
+            int i = 1;
+
+            for (i = 1; i <= numar; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    if (i % 3 == 0 && i % 5 != 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    if (i % 3 != 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+                
+        }
+
+    }
+}
